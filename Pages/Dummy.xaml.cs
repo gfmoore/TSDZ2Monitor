065242b5f7b986e70a62331897b746781239bd0d
Gordon Moore <gmgit@gordonmoore.co.uk>
2022-06-08 10:11:49 +0100

    Menu almost working as required

---
+++ Pages/Dummy.xaml.cs
@@ -0,0 +1,9 @@
+namespace TSDZ2Monitor.Pages;
+
+public partial class Dummy : ContentPage
+{
+	public Dummy()
+	{
+		InitializeComponent();
+	}
+}
--- ViewModels/ControlMenuViewModel.cs
+++ ViewModels/ControlMenuViewModel.cs
@@ -14,12 +14,46 @@ public partial class ControlMenuViewModel : ObservableObject
   [ObservableProperty]
   bool showGraphParameters = false;
 
-  public ICommand ShowParametersCommand => new Command(ChangeMenuControl);
-  public void ChangeMenuControl()
+  public ICommand ShowMainMenuCommand => new Command(ShowMainMenuControl);
+  public void ShowMainMenuControl()
   {
-    Console.WriteLine($"Before {ShowMainMenu} {ShowParameters} {ShowAssistLevels} {ShowGraphParameters}");
+    ShowMainMenu = false;
+    ShowParameters = false;
+    ShowAssistLevels = false;
+    ShowGraphParameters = true;
+  }
+
+  public ICommand ShowParametersCommand => new Command(ShowParametersControl);
+  public void ShowParametersControl()
+  {
     ShowMainMenu = !ShowMainMenu;
     ShowParameters = !ShowParameters;
-    Console.WriteLine($"After  {ShowMainMenu} {ShowParameters}  {ShowAssistLevels} {ShowGraphParameters}");
+    ShowAssistLevels = false;
+    ShowGraphParameters = false;
+  }
+
+  public ICommand ShowAssistLevelsCommand => new Command(ShowAssistLevelControl);
+  public void ShowAssistLevelControl()
+  {
+    ShowMainMenu = false;
+    ShowParameters = !ShowParameters;
+    ShowAssistLevels = !ShowAssistLevels;
+    ShowGraphParameters = false;
+  }
+  public ICommand ShowGraphVariablesCommand => new Command(ShowGraphLevelsControl);
+  public void ShowGraphLevelsControl()
+  {
+    ShowMainMenu = false;
+    ShowParameters = !ShowParameters;
+    ShowAssistLevels = false;
+    ShowGraphParameters = !ShowGraphParameters;
+    //AppShell.Current.CurrentItem = null;
+    AppShell.Current
+  }
+
+
+  private void DisplayMenuVars()
+  {
+    Console.WriteLine($"{ShowMainMenu} {ShowParameters} {ShowAssistLevels} {ShowGraphParameters}");
   }
 }
